c87f4cf8b9f22d849eb244b7bcfd667d40e4d792
Alan Santos <alansods@gmail.com>
2022-01-31 12:35:21 -0300

    css

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Calculador IMC</h1>
+      <span>Vamos calcular seu IMC.</span>
+
+      <div className="area-input">
+      <input
+      type="text"
+      placeholder="Peso em (kg) Ex.: 80kg"
+      />
+      <input
+      type="text"
+      placeholder="Altura em (cm) Ex.: 180cm"
+      />
+      <button>Calcular</button>
+      </div>
+      <h2>Seu IMC foi 25. Você está acima do peso.</h2>
     </div>
   );
 }
